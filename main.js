c50f0db19721e21b7215b5c226c52c2323454c98
stojan.milosev <stojan.milosev@yahoo.com>
2017-07-27 12:30:35 +0200

    added JS funcionality

---
+++ main.js
@@ -0,0 +1,14 @@
+function progress() {
+  var elem = document.getElementById("bar");   
+  var width = 10;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+      elem.innerHTML = width * 1  + '%';
+    }
+  }
+}
